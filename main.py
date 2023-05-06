8aa13cfca1bed5f7e93b36b1b45785ff23304ec1
hmazzas <124715795+hmazzas@users.noreply.github.com>
2023-05-06 05:30:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from functools import wraps
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = ''
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
